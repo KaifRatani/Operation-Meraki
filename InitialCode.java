acb4de560e92339a323cc96e4b716b566813fab6
Kaif Ratani <kaifratani17@gmail.com>
2025-03-20 16:13:29 -0400

    Update InitialCode.java
    This is a test push

--- InitialCode.java
+++ InitialCode.java
@@ -1,6 +1,8 @@
-public class InitialCode{
+public class InitialCode  {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Michael push this code.");
+        System.out.println("kaif push this code.");
+        
     }
 }
