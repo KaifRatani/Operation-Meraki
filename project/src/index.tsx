d2665eb948b49ea3f2cc0546e088adf5ec4095b2
Kaif Ratani <83687982+KaifRatani@users.noreply.github.com>
2025-05-20 18:05:59 -0400

    Delete project directory

--- project/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { HomeVersion } from "./screens/HomeVersion";
-
-createRoot(document.getElementById("app") as HTMLElement).render(
-  <StrictMode>
-    <HomeVersion />
-  </StrictMode>,
-);
