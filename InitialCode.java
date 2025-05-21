8a767424edb7894571fe1a62b17b4ee85138103c
Ken70-afk <Abhinavranjith47@gmail.com>
2025-05-21 10:57:46 -0400

    Remove all files from kaif

--- InitialCode.java
+++
@@ -1,8 +0,0 @@
-public class InitialCode {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Michael push this code.");
-        System.out.println("kaif push this code.");
-
-    }
-}
