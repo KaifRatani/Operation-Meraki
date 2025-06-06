82cdcf7785cab0ef1dec29468ddd8abd56d7a4a6
Kaif Ratani <83687982+KaifRatani@users.noreply.github.com>
2025-06-06 16:57:04 -0400

    Update server.js

--- Meraki-main/api/server.js
+++ Meraki-main/api/server.js
@@ -31,5 +31,5 @@ try {
 }
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ||8080||80;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
